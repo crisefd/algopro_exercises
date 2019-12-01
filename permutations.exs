1fb9a6da4761102e9f651259ec5d9660372f95f1
Cristhian Fuertes <cristhian.fuertes@alertlogic.com>
2019-11-30 19:13:11 -0500

    fourth commit

--- permutations.exs
+++ permutations.exs
@@ -1,20 +1,19 @@
 defmodule Permutations do
-	def run(list) do
-		list 
-		|> Enum.with_index()
-		|> Map.new(fn {item, index} -> {index, item}  end)
-		|> permute(0, length(list) - 1)
-		|> List.foldl( [], &( &1 ++ &2))
-	end
-
-	defp permute(map_list, start, fin ) when start == fin, do: Map.values(map_list)
-
-	defp permute(map_list, start, fin) do
-		for position <- start..fin do
-			%{ map_list | start => map_list[position], position => map_list[start] }
-			|> permute(start + 1, fin)
+	
+	def permute([]), do: [[]]
+	def permute(list) do
+		for item  <- list, rest <- permute(list -- [item]) do 
+			[item | rest] 
 		end
 	end
+
 end
 
-IO.inspect Permutations.run [1, 2, 3, 4]
+
+IO.inspect Permutations.permute [1, 2, 3]
+IO.inspect Permutations.permute [1, 2, 3, 4]
+IO.inspect Permutations.permute ["A", "B", "C", "D"]
+
+
+
+
